e855854bc242852b5143c73ccdff72692468b7c0
yunwoong <yunwoong7@gmail.com>
2023-02-03 16:55:39 +0900

    First Commit

---
+++ pororo/tasks/__init__.py
@@ -0,0 +1,20 @@
+# flake8: noqa
+"""
+__init__.py for import child .py files
+
+    isort:skip_file
+"""
+
+# Utility classes & functions
+import pororo.tasks.utils
+from pororo.tasks.utils.download_utils import download_or_load
+from pororo.tasks.utils.base import (
+    PororoBiencoderBase,
+    PororoFactoryBase,
+    PororoGenerationBase,
+    PororoSimpleBase,
+    PororoTaskGenerationBase,
+)
+
+# Factory classes
+from pororo.tasks.optical_character_recognition import PororoOcrFactory
